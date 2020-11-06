279edd4dd5fbd4877c400b75fb8e225736047b5c
ninofatec <rtamaio.jr@gmail.com>
2020-11-06 14:29:42 -0300

    teste commit

---
+++ pages/nossaequipe.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+      <div>
+      <b-carousel
+        id="carousel-no-animation"
+        style="text-shadow: 0px 0px 2px #000"
+        no-animation
+        indicators
+        img-width="1024"
+        img-height="480"
+      >
+        <b-carousel-slide
+          caption="Nossa equipe"
+          img-src="~/assets/losa.jpg"
+        ></b-carousel-slide>
+      </b-carousel>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
