fe46ca6b48da5db3b9a6412f9dbdcf5a3a03a96b
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-08 14:28:05 -0300

    GA

--- plugins/ga.js
+++ plugins/ga.js
@@ -28,7 +28,7 @@ export default ({ app }) => {
     /*
      ** Set the current page
      */
-    ga('create', 'G-1V8QYYT1MT', 'auto')
+    ga('create', '251362517', 'auto')
     /*
      ** Every time the route changes (fired on initialization too)
      */
