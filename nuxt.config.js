5ddebffa8b11cb391e1f9ab9e54e459b1ea16ea9
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-08 21:46:26 -0300

    Correção pligin GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
