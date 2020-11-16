d673e0fa36ddab5334f26da7e8f0c287c4fe6804
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-16 20:22:21 -0300

    Criação do component Card de apresentação

--- pages/index.vue
+++ pages/index.vue
@@ -24,47 +24,10 @@
         ></b-carousel-slide>
       </b-carousel>
     </div>
-        <b-container fluid class="updown">
-      <b-row  col sm="12" md="4" sg="4">>
-        <b-col>
-          <card
-            titulo="Berçário"
-            alt="card de apresentacao"
-            texto="Berçário é até qual idade? O berçário da escola Vilo Vicenzzi atende crianças 
-            dos 4 meses aos 2 anos de idade. Nossa instituição se dedica ao acompanhamento e cuidados das crianças. 
-            Com profissionais especializados para cuidar dos nossos alunos com muito amor e carinho, 
-            na ausência momentânea de seus pais."
-          ></card>
-        </b-col>
-
-        <b-col>
-          <card
-            titulo="Maternal"
-            alt="card de apresentacao"
-            texto="Nosso maternal atende crianças de 2 a 3 anos de idade. 
-            Contamos com 2 maternais um para cada ano de vida da criança, que são divididos por períodos 
-            conforme a idade. Trabalhamos com músicas, cantigas de roda e dança para estimular o 
-            desenvolvimento integral dos alunos. Contamos histórias infantis, 
-            desenvolvemos atividades lúdicas e trabalhamos com o corpo através de estímulos constantes.
-            Nossas propostas estão todas de acordo com a nova Base Nacional Comum Curricular."
-          ></card>
-        </b-col>
-
-        <b-col>
-          <card
-            titulo="Jardim de infância"
-            alt="card de apresentacao"
-            texto="O jardim de infância de nossa escola atende crianças a partir dos 4 anos de idade
-            e está dividido em 1ª e 2ª Etapas. Contribuímos para o desenvolvimento da linguagem e do 
-            vocabulário dos nossos alunos, pois é importante na aprendizagem da leitura e da escrita e
-            na aquisição de muitos outros conhecimentos. Também trabalhamos com jogos e brincadeiras, 
-            livres ou orientadas, de modo a promover o desenvolvimento integral das crianças. 
-            Os objetivos para essa fase são baseados nos campos de experiência e direitos de 
-            aprendizagem descritos na Base Nacional Comum Curricular."
-          ></card>
-        </b-col>
-      </b-row>
-    </b-container>
+      
+      <!-- Card apresentação da escola -->
+      <cardApresentation></cardApresentation>
+      <!-- Fim do Card apresentação da escola -->
         <div class="updown">
             <b-carousel
         id="carousel-no-animation"
@@ -418,10 +381,12 @@
 
 <script>
 import cardVue from "../components/card.vue";
+import CardApresentation from '../components/cardApresentation.vue';
 
 export default {
   components: {
-    card: cardVue
+    card: cardVue,
+    CardApresentation
   }
 };
 </script>
