a389e62914310ff596e4a093b1bcdeaa0e256ffa
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-16 18:35:14 -0300

    Adicionando Meta tags para SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Escola Municipal de Educação básica Vilo Vincenzzi' },
+      { hid: 'keywords', name: 'keywords', content: 'Escola, educação infantil, Taquaritinga, creche'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
