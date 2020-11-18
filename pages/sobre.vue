f7b1d7948a15170a7bad023736063d3e4940d3cd
ninofatec <rtamaio.jr@gmail.com>
2020-11-17 23:12:06 -0300

    Sobre

--- pages/sobre.vue
+++ pages/sobre.vue
@@ -17,46 +17,87 @@
       </b-carousel>
     </div>
     <div class="cor margemupdown">
-    <h2 class="textocentro margemupdown"><b>Somos uma escola de Educação Infantil da rede municipal de Taquaritinga</b></h2>
-<b-container fluid class="bv-example-row">
-  <b-row>
-    <b-col><h2>Nossa escola</h2>
-    <p>A Escola Municipal de Educação Básica Engenheiro Vilo Vicenzzi foi criada em 20 de março de 2015.</p>
-    <p>Contamos com uma equipe que ama o que faz, fazendo tudo com muito amor e carinho por nossos alunos..</p>
-    <h2>Turmas</h2>
-    <p><b>Educação infantil - Creche</b></p>
-    <p>Aulas no período da <b>Manhã</b></p>
-    <p>Números de turmas 3 / Média de alunos por turma: 15</p>
-    <p><b>Educação infantil - Pré-escola</b></p>
-    <p>Aulas no período da <b>Manhã</b></p>
-    <p>Número de turmas 2 / Média de alunos por turma: 25</p>
-    </b-col>
-    <b-col><h2>Infraestrutura</h2>
-    <p>• Alimentação escolar para os alunos.</p>
-    <p>• Água filtrada.</p>
-    <p>• Água da rede pública.</p>
-    <p>• Energia da rede pública.</p>
-    <p>• Esgoto da rede pública.</p>
-    <p>• Lixo destinado à coleta periódica;</p>
-    <p>• Acesso à Internet.</p>
-    <p>• Banda larga.</p>
-    </b-col>
-    <b-col><h2>Instalação de ensino</h2>
-    <p>• 7 salas de aulas.</p>
-    <p>• Sala de diretoria.</p>
-    <p>• Sala de professores.</p>
-    <p>• Cozinha.</p>
-    <p>• Banheiro adequado à educação infantil.</p>
-    <p>• Banheiro adequado à alunos com deficiência ou mobilidade reduzida.</p>
-    <p>• Sala de secretaria.</p>
-    <p>• Refeitório.</p>
-    <p>• Despensa.</p>
-    <p>• Almoxarifado.</p>
-    </b-col>
-  </b-row>
-</b-container>
-  </div>
-<div class="textocentro margemupdown"><h2><b>Fotos da nossa escola</b></h2></div>
+      <h2 class="textocentro margemupdown">
+        <b
+          >Somos uma escola de Educação Infantil da rede municipal de
+          Taquaritinga</b
+        >
+      </h2>
+      <b-container fluid class="bv-example-row">
+        <div>
+          <p></p>
+          <h2 class="textocentro margemupdown">
+            <b>Nossa Escola</b>
+          </h2>
+          <p>
+            A Escola Municipal de Educação Básica Engenheiro Vilo Vicenzzi foi
+            criada em 20 de março de 2015.
+          </p>
+          <p>
+            Contamos com uma equipe que ama o que faz, fazendo tudo com muito
+            amor e carinho por nossos alunos..
+          </p>
+        </div>
+        <div>
+          <p></p>
+          <h2 class="textocentro margemupdown">
+            <b>Turmas</b>
+          </h2>
+          <p>Educação infantil - Creche</p>
+          <p>Aulas no período da Manhã</p>
+          <p>Números de turmas 3 / Média de alunos por turma: 15</p>
+          <p>Educação infantil - Pré-escola</p>
+          <p>Aulas no período da Manhã</p>
+          <p>Número de turmas 2 / Média de alunos por turma: 25</p>
+        </div>
+
+        <div>
+          <h2 class="textocentro margemupdown">
+            <b>Infraestrutura</b>
+          </h2>
+
+          <b-container fluid class="bv-example-row justify-content-evenly">
+            <b-row>
+              <b-col>
+                <p>• Alimentação escolar para os alunos.</p>
+                <p>• Água filtrada.</p>
+                <p>• Água da rede pública.</p>
+                <p>• Energia da rede pública.</p>
+                          <p>• Sala de secretaria.</p>
+                                    <p>• Almoxarifado.</p>
+                </b-col
+              >
+
+              <b-col>
+                <p>• Esgoto da rede pública.</p>
+                <p>• Lixo destinado à coleta periódica;</p>
+                <p>• Acesso à Internet.</p>
+                <p>• Banda larga.</p>
+                          <p>
+            • Banheiro adequado à alunos com deficiência ou mobilidade reduzida.
+          </p>
+                    <p>• Despensa.</p>
+          </b-col
+              >
+
+              <b-col> 
+                          <p>• 7 salas de aulas.</p>
+          <p>• Sala de diretoria.</p>
+          <p>• Sala de professores.</p>
+          <p>• Cozinha.</p>
+                    <p>• Banheiro adequado à educação infantil.</p>
+                              <p>• Refeitório.</p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </div>
+
+
+      </b-container>
+    </div>
+    <div class="textocentro margemupdown">
+      <h2><b>Fotos da nossa escola</b></h2>
+    </div>
 
     <div class="card-header1 corpadrao1">
       <b-card
@@ -374,7 +415,7 @@ export default {
   data() {
     return {
       slide: 0,
-      sliding: null,
+      sliding: null
     };
   },
   methods: {
@@ -383,11 +424,10 @@ export default {
     },
     onSlideEnd(slide) {
       this.sliding = false;
-    },
-  },
+    }
+  }
 };
 </script>
-
 
 <script>
 export default {};
@@ -399,14 +439,11 @@ export default {};
   color: red;
 }
 
-
-
-.textocentro{
+.textocentro {
   text-align: center;
 }
 
-.margemupdown
-{
+.margemupdown {
   margin-bottom: 40px;
   margin-top: 40px;
 }
