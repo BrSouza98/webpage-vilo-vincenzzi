a3a66a352dd6f0ab8ca7ad26f938d4c058fcac1c
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-19 01:47:09 -0300

    i18n

---
+++ lang/en.js
@@ -0,0 +1,6 @@
+export default {
+    title: 'Talk with us',
+    adress: 'Av Cap. José Camargo Lima, 341, Jardim São Sebastião Taquaritinga/SP.',
+    open: 'Opening hours: 07h ás 17h.',
+    email: 'vilovicenzzi@gmail.com',
+  }
---
+++ lang/pt.js
@@ -0,0 +1,6 @@
+export default {
+    title: 'Fale conosco',
+    adress: 'Av Cap. José Camargo Lima, 341, Jardim São Sebastião Taquaritinga/SP.',
+    open: 'Horario de funcionamento: 07h ás 17h.',
+    email: 'vilovicenzzi@gmail.com',
+  }
