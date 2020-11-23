516fd492b2aac2b27266b097d7d681b790fde5bf
ninofatec <rtamaio.jr@gmail.com>
2020-11-23 08:33:50 -0300

    commit links

--- components/navbar.vue
+++ components/navbar.vue
@@ -16,15 +16,15 @@
             <div class="px-3 py-2">
               <b-nav class="ml-auto">
                 <b-nav class="d-block">
-                  <b-nav-item href="/sobre/">Sobre</b-nav-item>
-                  <b-nav-item href="/metodologia/">Metodologia</b-nav-item>
-                  <b-nav-item href="/nossaequipe/">Nossa equipe</b-nav-item>
+                  <b-nav-item href="../sobre/">Sobre</b-nav-item>
+                  <b-nav-item href="../metodologia/">Metodologia</b-nav-item>
+                  <b-nav-item href="../nossaequipe/">Nossa equipe</b-nav-item>
                   <b-nav-item
                     href="https://vilovicenzzi.wordpress.com/"
                     target="_blank"
                     >Blog</b-nav-item
                   >
-                  <b-nav-item href="/fale-conosco/">Fale conosco</b-nav-item>
+                  <b-nav-item href="../fale-conosco/">Fale conosco</b-nav-item>
                 </b-nav>
               </b-nav>
             </div>
@@ -35,15 +35,15 @@
           <div class="px-3 py-2">
             <b-nav class="ml-auto">
               <b-nav class="d-flex">
-                <b-nav-item href="/sobre/">Sobre</b-nav-item>
-                <b-nav-item href="/metodologia/">Metodologia</b-nav-item>
-                <b-nav-item href="/nossaequipe/">Nossa equipe</b-nav-item>
+                <b-nav-item href="../sobre/">Sobre</b-nav-item>
+                <b-nav-item href="../metodologia/">Metodologia</b-nav-item>
+                <b-nav-item href="../nossaequipe/">Nossa equipe</b-nav-item>
                 <b-nav-item
                   href="https://vilovicenzzi.wordpress.com/"
                   target="_blank"
                   >Blog</b-nav-item
                 >
-                <b-nav-item href="/fale-conosco/">Fale conosco</b-nav-item>
+                <b-nav-item href="../fale-conosco/">Fale conosco</b-nav-item>
               </b-nav>
             </b-nav>
           </div>
