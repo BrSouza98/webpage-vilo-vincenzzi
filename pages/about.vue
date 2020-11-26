1ff6901fd9236703e3e301b4d8355769c52acd1e
ninofatec <rtamaio.jr@gmail.com>
2020-11-26 10:09:27 -0300

    Pagina criada About

---
+++ pages/about.vue
@@ -0,0 +1,461 @@
+<template>
+  <div>
+    <div>
+      <img src="~/assets/sobrenos.png" alt="" class="bannersConteudo" />
+    </div>
+    <div class="cor margemupdown">
+      <h2 class="textocentro margemupdown">
+        <b
+          >Somos uma escola de Educação Infantil da rede municipal de
+          Taquaritinga</b
+        >
+      </h2>
+      <b-container fluid class="bv-example-row">
+        <div>
+          <p></p>
+          <h2 class="textocentro margemupdown">
+            <b>Nossa Escola</b>
+          </h2>
+          <p>
+            A Escola Municipal de Educação Básica Engenheiro Vilo Vicenzzi foi
+            criada em 20 de março de 2015.
+          </p>
+          <p>
+            Contamos com uma equipe que ama o que faz, fazendo tudo com muito
+            amor e carinho por nossos alunos..
+          </p>
+        </div>
+        <div>
+          <p></p>
+          <h2 class="textocentro margemupdown">
+            <b>Turmas</b>
+          </h2>
+          <p>Educação infantil - Creche</p>
+          <p>Aulas no período da Manhã</p>
+          <p>Números de turmas 3 / Média de alunos por turma: 15</p>
+          <p>Educação infantil - Pré-escola</p>
+          <p>Aulas no período da Manhã</p>
+          <p>Número de turmas 2 / Média de alunos por turma: 25</p>
+        </div>
+
+        <div>
+          <h2 class="textocentro margemupdown">
+            <b>Infraestrutura</b>
+          </h2>
+
+          <b-container fluid class="bv-example-row justify-content-evenly">
+            <b-row>
+              <b-col>
+                <p>• Alimentação escolar para os alunos.</p>
+                <p>• Água filtrada.</p>
+                <p>• Água da rede pública.</p>
+                <p>• Energia da rede pública.</p>
+                <p>• Sala de secretaria.</p>
+                <p>• Almoxarifado.</p>
+              </b-col>
+
+              <b-col>
+                <p>• Esgoto da rede pública.</p>
+                <p>• Lixo destinado à coleta periódica;</p>
+                <p>• Acesso à Internet.</p>
+                <p>• Banda larga.</p>
+                <p>
+                  • Banheiro adequado à alunos com deficiência ou mobilidade
+                  reduzida.
+                </p>
+                <p>• Despensa.</p>
+              </b-col>
+
+              <b-col>
+                <p>• 7 salas de aulas.</p>
+                <p>• Sala de diretoria.</p>
+                <p>• Sala de professores.</p>
+                <p>• Cozinha.</p>
+                <p>• Banheiro adequado à educação infantil.</p>
+                <p>• Refeitório.</p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </div>
+      </b-container>
+    </div>
+    <div class="textocentro margemupdown">
+      <h2><b>Fotos da nossa escola</b></h2>
+    </div>
+
+    <div class="card-header1 corpadrao1">
+      <b-card
+        title=""
+        img-src="~/assets/foto1.jpg"
+        img-alt="Refeitorio com duas meses e uma cadeirinha"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto1.jpg"
+            target="_blank"
+            >Refeitorio</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto2.jpg"
+        img-alt="Refeitorio com 3 mesas "
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto2.jpg"
+            target="_blank"
+            >Refeitorio</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto3.jpg"
+        img-alt="Sala de aulas com diversa carteiras incluindo a do professor"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto3.jpg"
+            target="_blank"
+            >Jardim de infância 1</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto4.jpg"
+        img-alt="Brinquedoteca super colorida com diversas cores e com varos lugares com brinquedos guardados"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto4.jpg"
+            target="_blank"
+            >Brinquedoteca</a
+          >
+        </b-card-text>
+      </b-card>
+    </div>
+
+    <div class="card-header1 corpadrao1">
+      <b-card
+        title=""
+        img-src="~/assets/foto5.jpg"
+        img-alt="Brinquedoteca com diversas cores, uma estante de livros e a porta de vidro"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto5.jpg"
+            target="_blank"
+            >Brinquedoteca</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto6.jpg"
+        img-alt="Brinquedoteca com diversas cores,alguns cochonetes e uma janela"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto6.jpg"
+            target="_blank"
+            >Brinquedoteca</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto7.jpg"
+        img-alt="Sala com diveros berçarios brancos"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto7.jpg"
+            target="_blank"
+            >Sala do berçario</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto8.jpg"
+        img-alt="Sala com diveros berçarios brancos e uma janela"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto8.jpg"
+            target="_blank"
+            >Sala do berçario</a
+          >
+        </b-card-text>
+      </b-card>
+    </div>
+    <div class="card-header1 corpadrao1">
+      <b-card
+        title=""
+        img-src="~/assets/foto9.jpg"
+        img-alt="Banheiro do berçario com foco nos lavatorios"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto9.jpg"
+            target="_blank"
+            >Banheiro do berçario</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto10.jpg"
+        img-alt="Banheiro do berçario com o foco nas privadas"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto10.jpg"
+            target="_blank"
+            >Banheiro do berçario</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto11.jpg"
+        img-alt="Banheiro do berçario com foco na pia"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto11.jpg"
+            target="_blank"
+            >Banheiro do berçario</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto12.jpg"
+        img-alt="Playground com diveros cavalinhos e um escorregador,alem de bancos"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto12.jpg"
+            target="_blank"
+            >Playground</a
+          >
+        </b-card-text>
+      </b-card>
+    </div>
+    <div class="card-header1 corpadrao1">
+      <b-card
+        title=""
+        img-src="~/assets/foto13.jpg"
+        img-alt="Playground com cavalinhos, piscina de bolinhas, e dois escorregadores"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto13.jpg"
+            target="_blank"
+            >Playground</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto14.jpg"
+        img-alt="Playground com o foco num grande circuito de escorregadores"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto14.jpg"
+            target="_blank"
+            >Playground</a
+          >
+        </b-card-text>
+      </b-card>
+      <b-card
+        title=""
+        img-src="~/assets/foto17.jpg"
+        img-alt="Playground com foco em duas arvores"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto17.jpg"
+            target="_blank"
+            >Playground</a
+          >
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        title=""
+        img-src="~/assets/foto16.jpg"
+        img-alt="Playground com o foco num grande circuito de escorregadores"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 align"
+      >
+        <b-card-text>
+          <a
+            class="corpreta"
+            href="http://localhost:3000/_nuxt/assets/foto16.jpg"
+            target="_blank"
+            >Playground</a
+          >
+        </b-card-text>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
+  },
+};
+</script>
+
+<script>
+export default {};
+</script>
+
+<style>
+.texto {
+  text-align: right;
+  color: red;
+}
+
+.textocentro {
+  text-align: center;
+}
+
+.margemupdown {
+  margin-bottom: 40px;
+  margin-top: 40px;
+}
+
+.bannersConteudo {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  max-height: 500px;
+}
+
+.card-header1 {
+  padding: 1.75rem 1.25rem;
+  margin-bottom: 0;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+}
+
+.corpreta {
+  color: black;
+}
+
+.bannersConteudo {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  max-height: 650px;
+}
+</style>
