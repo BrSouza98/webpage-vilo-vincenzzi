66d56b214fb4794ab3d5102dbaefe8ed6056e9af
ninofatec <rtamaio.jr@gmail.com>
2020-11-26 23:48:56 -0300

    paagina metodologia

--- pages/metodologia.vue
+++ pages/metodologia.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
-       <img src="~/assets/metodologia.png" alt="" class="bannersConteudo" />
+    <img src="~/assets/metodologia.png" alt="" class="bannersConteudo" />
 
     <div class="">
       <b-container fluid class="configuraçãoPadrao">
         <b-row>
-          <b-col class="cor">
+          <b-col class="cor margemupdown1">
             <h2>Educação Infantil Grupo 2 (2 anos) e Grupo 3 (3 anos)</h2>
-            <ul>
-              <li class="margemupdown1">
-                Apostilas de tamanho e formato generosos (A3), com uma atividade
-                por página, imagens grandes e ilustrações de traçados grossos.
-              </li>
-              <li>
-                Folhas de maior espessura e tamanho exclusivo, para o melhor
-                manuseio pelas crianças nessa faixa etária.
-              </li>
-              <li>
-                Folha de abertura com tema que possibilita atividades envolvendo
-                artes, dramatizações, músicas, etc.
-              </li>
-            </ul>
+          </b-col>
+        </b-row>
+      </b-container>
+
+      <b-container fluid class="bv-example-row">
+        <b-row>
+          <b-col>
             <p>
               <b>Matemática:</b> contagem, noção de quantidade, formas
               geométricas e percepção visual de cores.
@@ -38,157 +31,222 @@
               <b>Datas comemorativas:</b> conhecimento de datas importantes no
               Brasil e no mundo.
             </p>
+            <ul>
+              <li class="margemupdown1">
+                Apostilas de tamanho e formato generosos (A3), com uma atividade
+                por página, imagens grandes e ilustrações de traçados grossos.
+              </li>
+              <li>
+                Folhas de maior espessura e tamanho exclusivo, para o melhor
+                manuseio pelas crianças nessa faixa etária.
+              </li>
+              <li>
+                Folha de abertura com tema que possibilita atividades envolvendo
+                artes, dramatizações, músicas, etc.
+              </li>
+            </ul></b-col
+          >
+
+          <b-col>
             <div
               class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
             >
               <img
-                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded"
+                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded imgtamanho"
                 src="~/assets/positivo1.jpg"
                 alt="Livros usados para a educação das crianças e a caixa onde eles se encontram em cima de uma mesa"
-              />
-            </div>
-          </b-col>
+              /></div
+          ></b-col>
         </b-row>
       </b-container>
     </div>
 
-    <div>
-      <b-container fluid class="configuraçãoPadrao">
-        <b-row>
-          <b-col class="cor">
-            <h2>Grupo 2 - Maternal I e Grupo 3 - Maternal II</h2>
-            <p>
-              A prática da escola Vilo Vincenzzi com os pequenos é de respeito à infância e ao cuidado físico, 
-              emocional e cognitivo. Eles são incentivados, diariamente, 
-              a participar de desafios e descobertas e a conhecer a si mesmos, ao outro e ao mundo. 
-              Para promover o desenvolvimento e a aprendizagem da criança, 
-              o trabalho da apostila Aprende Brasil da Editora Positivo tem quatro eixos permeados 
-              pela afetividade e brincadeiras:
-            </p>
-            <div
-              class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
-            >
-              <img
-                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded"
-                src="~/assets/positivo2.jpg"
-                alt="Circulo com 4 adendos
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col
+          ><h2>Grupo 2 - Maternal I e Grupo 3 - Maternal II</h2>
+          <p>
+            A prática da escola Vilo Vincenzzi com os pequenos é de respeito à
+            infância e ao cuidado físico, emocional e cognitivo. Eles são
+            incentivados, diariamente, a participar de desafios e descobertas e
+            a conhecer a si mesmos, ao outro e ao mundo. Para promover o
+            desenvolvimento e a aprendizagem da criança, o trabalho da apostila
+            Aprende Brasil da Editora Positivo tem quatro eixos permeados pela
+            afetividade e brincadeiras:
+          </p></b-col
+        >
+        <b-col
+          ><div
+            class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
+          >
+            <img
+              class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded imgtamanho"
+              src="~/assets/positivo2.jpg"
+              alt="Circulo com 4 adendos
                 dentro do circulo esta escrito: Multiplas linguagens-Os professores trabalham as habilidades do aluno por meio de experiencias, para que a criança desenvolva múltiplas linguagens.
                 adendo Arte- Desenvolve a sensibilidade estetica, a imaginação e a criatividade.
                 adendo Pesquisa- Proporciona a descoberta do mundo, além de despertar a curiosidade, o levantamento de hipoteses e a superação de desafios, trazendo novos aprendizados
                 adendo Jogos- Promovem a comunicação e a socilização e desenvolvem a empatia, o compartilhar e o racicinio lógico. Além disso,contribuem para formação da inteligencia emocional e do senso de coletividade.
                 adendo Histórias- Estimulam a imaginação, o faz de conta, a oralidade e o contato com a literatura.
                 "
-              />
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+            /></div
+        ></b-col>
+      </b-row>
+    </b-container>
 
-    <div>
-      <b-container fluid class="configuraçãoPadrao">
-        <b-row>
-          <b-col class="cor">
-            <h2>Educação Infantil Grupo 4 (4 anos) e Grupo 5 (5 anos)</h2>
-            <ul>
-              <li>
-               Apostilas divididas em unidades com páginas de abertura, que possibilitam projetos pedagógicos.
-              </li>
-              <li>
-                Atividades por sequências didáticas que estimulam a exploração por parte dos alunos.
-              </li>
-            </ul>
-            <p>
-              <b>Matemática:</b> retomada e ampliação da contagem, reconhecimento de números, quantidades e geometria.
-            </p>
-            <p>
-              <b>Linguagem e Letramento:</b> contatos com os signos escritos. Relato oral de experiências, recontagem de fatos e histórias, produção de textos coletivos.
-            </p>
-            <p><b>Descobertas:</b> conceitos básicos de Ciências, História e Geografia. Estudo de hábitos de higiene, corpo, plantas, animais, ambiente, entre outros.</p>
-            <p>
-              <b>Movimento:</b>  desenvolvimento da coordenação motora fina e ampla e esquema corporal.
-            </p>
-            <p>
-              <b>Artes:</b> desenvolvimento da expressão, equilíbrio, autoestima, autoconhecimento e integração social.
-            </p>
-            <div
-              class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
-            >
-              <img
-                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded"
-                src="~/assets/positivo12.png"
-                alt="Os diversos livros usados na educação das crianças"
-              />
-            </div>
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>
+          <h2>Educação Infantil Grupo 4 (4 anos) e Grupo 5 (5 anos)</h2>
+          <ul>
+            <li>
+              Apostilas divididas em unidades com páginas de abertura, que
+              possibilitam projetos pedagógicos.
+            </li>
+            <li>
+              Atividades por sequências didáticas que estimulam a exploração por
+              parte dos alunos.
+            </li>
+          </ul>
+          <p>
+            <b>Matemática:</b> retomada e ampliação da contagem, reconhecimento
+            de números, quantidades e geometria.
+          </p>
+          <p>
+            <b>Linguagem e Letramento:</b> contatos com os signos escritos.
+            Relato oral de experiências, recontagem de fatos e histórias,
+            produção de textos coletivos.
+          </p>
+          <p>
+            <b>Descobertas:</b> conceitos básicos de Ciências, História e
+            Geografia. Estudo de hábitos de higiene, corpo, plantas, animais,
+            ambiente, entre outros.
+          </p>
+          <p>
+            <b>Movimento:</b> desenvolvimento da coordenação motora fina e ampla
+            e esquema corporal.
+          </p>
+          <p>
+            <b>Artes:</b> desenvolvimento da expressão, equilíbrio, autoestima,
+            autoconhecimento e integração social.
+          </p></b-col
+        >
 
-            <div>
-      <b-container fluid class="configuraçãoPadrao">
-        <b-row>
-          <b-col class="cor">
-            <h2>Grupo 4 - Jardim I e Grupo 5 - Jardim II</h2>
-            <p>
-             PLANO DE ENSINO JARDIM 1 – 3 a 4 anos:
-            </p>
-            <ul>
-              <li>
-                Aprender a expressar seus desejos, sentimentos, vontades e desagravos e a agir com progressiva autonomia
-              </li>
-              <li>Interessar-se progressivamente pelo cuidado com o próprio corpo, executando ações simples relacionadas à saúde e à higiene.</li>
-              <li>Relacionar-se progressivamente com as crianças, com seus professores e com outros adultos.</li>
-              <li>Ter uma imagem positiva de si, ampliando sua autoconfiança.</li>
-              <li>Enfrentar situações de conflito, utilizando seus recursos pessoais, respeitando as outras crianças e os adultos e exigindo reciprocidade.</li>
-              <li>Ouvir e contar fatos, ocorridos, falar de sentimentos.</li>
-              <li>Recontar histórias conhecidas por meio de deferentes portadores de textos e vivências de diversas situações, nas quais se faz necessária, compreendendo a sua função social.</li>
-              <li>Tentar ler e escrever segundo suas hipóteses.</li>
-              <li>econhecer, compreender e utilizar os diferentes símbolos convencionalmente como forma de representação em textos lidos e ouvidos.</li>
-              <li>Utilizar-se da linguagem matematicamente.</li>
-              <li>Construir o significado dos números naturais em situações de contagem, medida e códigos numéricos, nos diferentes contextos: social, matemáticos e outros.</li>
-              <li>Perceber, a partir de acontecimentos , a noção de tempo, tendo como referência a sua rotina diária.</li>
-              <li>Reconhecer, na paisagem local e no lugar em que se encontram inseridos, as diferentes manifestações da natureza presentes em outras paisagens.</li>
-              <li>Comparar seres vivos, estabelecendo diferenças e semelhanças, quanto à alimentação, à respiração, ao crescimento e à reprodução.</li>
-            </ul>
-            <div
-              class="d-flex flex-wrap align-items-center justify-content-center"
-            >
-              <img
-                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded"
-                src="~/assets/positivo100.jpg"
-                alt=""
-              />
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+        <b-col>
+          <div
+            class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
+          >
+            <img
+              class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded imgtamanho"
+              src="~/assets/positivo12.png"
+              alt="Os diversos livros usados na educação das crianças"
+            /></div
+        ></b-col>
+      </b-row>
+    </b-container>
 
-    <div>
-      <b-container fluid class="configuraçãoPadrao">
-        <b-row>
-          <b-col class="cor">
-            <h2 class="margemupdown1">Educação Infantil I e II - 2 e 3 anos</h2>
-            <h2 class="margemupdown1">Apostilas Positivo</h2>
-            <p>
-              As apostilas para este segmento são entregues em 8 distribuições e
-              possuem formato diferenciado, apropriado às fases iniciais da
-              infância, o que fornece apoio indispensável às atividades
-              realizadas em aula. Elas trazem ícones que identificam os eixos
-              cognitivos relacionados a cada campo de experiência envolvidos em
-              cada matéria, e trabalham conteúdos de forma gradativa,
-              apresentando e retomando conceitos e habilidades numa linha de
-              aprendizado dinâmica que se torna interessante e desafiadora para
-              o aluno.
-            </p>
-            <div
-              class="d-flex flex-wrap align-items-center justify-content-center margemupdown1"
-            >
-              <img
-                class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded"
-                src="~/assets/positivo3.jpg"
-                alt="Os eixos cognitvos
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>
+          <h2>Grupo 4 - Jardim I e Grupo 5 - Jardim II</h2>
+          <p>
+            PLANO DE ENSINO JARDIM 1 – 3 a 4 anos:
+          </p>
+          <ul>
+            <li>
+              Aprender a expressar seus desejos, sentimentos, vontades e
+              desagravos e a agir com progressiva autonomia
+            </li>
+            <li>
+              Interessar-se progressivamente pelo cuidado com o próprio corpo,
+              executando ações simples relacionadas à saúde e à higiene.
+            </li>
+            <li>
+              Relacionar-se progressivamente com as crianças, com seus
+              professores e com outros adultos.
+            </li>
+            <li>
+              Ter uma imagem positiva de si, ampliando sua autoconfiança.
+            </li>
+            <li>
+              Enfrentar situações de conflito, utilizando seus recursos
+              pessoais, respeitando as outras crianças e os adultos e exigindo
+              reciprocidade.
+            </li>
+            <li>
+              Ouvir e contar fatos, ocorridos, falar de sentimentos.
+            </li>
+            <li>
+              Recontar histórias conhecidas por meio de deferentes portadores de
+              textos e vivências de diversas situações, nas quais se faz
+              necessária, compreendendo a sua função social.
+            </li>
+            <li>Tentar ler e escrever segundo suas hipóteses.</li>
+            <li>
+              econhecer, compreender e utilizar os diferentes símbolos
+              convencionalmente como forma de representação em textos lidos e
+              ouvidos.
+            </li>
+            <li>Utilizar-se da linguagem matematicamente.</li>
+            <li>
+              Construir o significado dos números naturais em situações de
+              contagem, medida e códigos numéricos, nos diferentes contextos:
+              social, matemáticos e outros.
+            </li>
+            <li>
+              Perceber, a partir de acontecimentos , a noção de tempo, tendo
+              como referência a sua rotina diária.
+            </li>
+            <li>
+              Reconhecer, na paisagem local e no lugar em que se encontram
+              inseridos, as diferentes manifestações da natureza presentes em
+              outras paisagens.
+            </li>
+            <li>
+              Comparar seres vivos, estabelecendo diferenças e semelhanças,
+              quanto à alimentação, à respiração, ao crescimento e à reprodução.
+            </li>
+          </ul></b-col
+        >
+
+        <b-col
+          ><div
+            class="d-flex flex-wrap align-items-center justify-content-center"
+          >
+            <img
+              class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded imgtamanho"
+              src="~/assets/positivo100.jpg"
+              alt=""
+            /></div
+        ></b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>
+          <h2 class="margemupdown1">Educação Infantil I e II - 2 e 3 anos</h2>
+          <h2 class="margemupdown1">Apostilas Positivo</h2>
+          <p>
+            As apostilas para este segmento são entregues em 8 distribuições e
+            possuem formato diferenciado, apropriado às fases iniciais da
+            infância, o que fornece apoio indispensável às atividades realizadas
+            em aula. Elas trazem ícones que identificam os eixos cognitivos
+            relacionados a cada campo de experiência envolvidos em cada matéria,
+            e trabalham conteúdos de forma gradativa, apresentando e retomando
+            conceitos e habilidades numa linha de aprendizado dinâmica que se
+            torna interessante e desafiadora para o aluno.
+          </p></b-col
+        >
+
+        <b-col
+          ><div
+            class="d-flex flex-wrap align-items-center justify-content-center margemupdown1 "
+          >
+            <img
+              class="mt-3 card-colaborador shadow-lg p-3 mb-5 bg-white rounded imgtamanho"
+              src="~/assets/positivo3.jpg"
+              alt="Os eixos cognitvos
                 Arte, atitudes, cuidados pessoais, datas comemorativas, descobertas, independencia e autonomia, inglês, interação, jogos e desafios, leituras extraordinarias, linguagem e letramento, matemática, movimento, música, natureza e liberdade, oralidade, raciocinio lógico e socialização
 
                 Tambem apresenta os icones dos Campos de Experiencias da BNCC
@@ -198,37 +256,29 @@
                 Traços, sons, cores e formas.
                 Escuta, fala, pensamento e imaginação.
                 "
-              />
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+            /></div
+        ></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default
-
-{};
+export default {};
 </script>
 
 <style scoped>
-h2{
+h2 {
   text-align: center;
   margin-top: 20px;
   margin-bottom: 15px;
 }
 
-
-
-img{
+img {
   border-radius: 3px;
-  
 }
 
-.margemupdown1
-{
+.margemupdown1 {
   margin-bottom: 40px;
   margin-top: 40px;
 }
@@ -239,5 +289,9 @@ img{
   margin: 0;
   max-height: 650px;
 }
-</style>
 
+.imgtamanho {
+  width: 100%;
+  height: 100%;
+}
+</style>
