3ea216e64c9fe943e999318f0668c05ffe373107
antoniogambogi <antoniogambogi88@gmail.com>
2020-11-29 21:43:06 -0300

    remoção do about.

--- components/navbar.vue
+++ components/navbar.vue
@@ -29,7 +29,6 @@
               <div class="px-3 py-2">
                 <b-nav class="ml-auto">
                   <b-nav class="d-block">
-                    <b-nav-item href="../about/">About</b-nav-item>
                     <b-nav-item href="../sobre/">Sobre</b-nav-item>
                     <b-nav-item href="../metodologia/">Metodologia</b-nav-item>
                     <b-nav-item href="../nossaequipe/">Nossa equipe</b-nav-item>
@@ -51,7 +50,6 @@
             <div class="px-3 py-2">
               <b-nav class="ml-auto">
                 <b-nav class="d-flex">
-                  <b-nav-item href="../about/">About</b-nav-item>
                   <b-nav-item href="../sobre/">Sobre</b-nav-item>
                   <b-nav-item href="../metodologia/">Metodologia</b-nav-item>
                   <b-nav-item href="../nossaequipe/">Nossa equipe</b-nav-item>
